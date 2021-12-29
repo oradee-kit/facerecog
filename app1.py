1a77f45b37556d7f8621d79a08c8ba8c27b11247
oradee-kit <79465736+oradee-kit@users.noreply.github.com>
2021-12-29 13:07:40 +0700

    Add files via upload

--- app1.py
+++ app1.py
@@ -7,6 +7,19 @@ import pickle
 import face_recognition
 
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+cred = credentials.Certificate("serviceAccountKey.json")
+#firebase_admin.initialize_app(cred)
+
+db=firestore.client()
+# db.collection("person").add({"name" : "John" , "Age":40})
+
+
+
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -19,9 +32,10 @@ model = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.da
 
 def main():
     # Header of page
+    inputname = ""
     st.header('Face Recognition Register')
     st.text_input('Enter Your Student ID')
-    st.text_input('Enter Your Name')
+    inputname = st.text_input('Enter Your Name')
     uploaded_file = st.file_uploader('Face uploader')
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
@@ -31,9 +45,9 @@ def main():
         card = Image.open(uploaded_card)
 
     if st.button('Train'):
-        train(image)
+        train(image,inputname)
         st.write('image trained')
-        train(card)
+        train(card,inputname)
         st.write("card trained")
 
     if st.button("compare"):
@@ -62,7 +76,7 @@ def recog():
 def run_cam(run):
     cam = cv2.VideoCapture(0)
     FRAME_WINDOW = st.image([])
-    FACE_DESC, FACE_NAME = pickle.load(open('trainset1.pk', 'rb'))
+    FACE_DESC, FACE_NAME = pickle.load(open('trainsetfin.pk, 'rb'))
 
     while run:
         ret, frame = cam.read()
@@ -73,6 +87,15 @@ def run_cam(run):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_detector.detectMultiScale(gray, 1.1, 4)
         # new_frame_time = time.time()
+
+        near = [1,1,1]
+        name_show = ["name","name"]
+
+        db_result = db.collection("facedesc").get()
+        db_result_array = []
+
+        for i in db_result :
+            db_result_array.append(i.to_dict())
 
         for (x, y, w, h) in faces:
             img = frame[y - 10:y + h + 10, x - 10:x + w + 10][:, :, ::-1]
@@ -86,22 +109,33 @@ def run_cam(run):
                     d.append(np.linalg.norm(np.array(face_desc) - np.array(face_desc0)))
                 d = np.array(d)
                 idx = np.argmin(d)
+                near.append(d[idx])
+
 
                 if d[idx] < 0.5:
                     name = FACE_NAME[idx]
                     print(name)
+                    name_show.append(name)
                     cv2.putText(frame, name, (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    #cv2.putText(frame, "similarlity: " + str((1 - near[len(near) - 1]) * 100), (x, y + 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+
+
+
                 else:
                     print("Unknow", d[idx])
                     cv2.putText(frame, "Unknow", (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         FRAME_WINDOW.image(frame)
-    else:
-        st.write('Stopped')
 
-def train(image):
+        # st.write("similarlity: ", (1 - near[len(near) - 1]) * 100)
+        # st.write(name_show[len(name_show) - 1])
+
+    # else:
+    #     st.write('Stopped')
+
+def train(image,input_n):
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Face Description...")
@@ -112,13 +146,18 @@ def train(image):
     for k, d in enumerate(dets):
         shape = sp(img, d)
         face_desc = model.compute_face_descriptor(img, shape, 10)
-        # if face_desc is not None:
-        #     st.write(face_desc)
-        # else:
-        #     st.write("no face found")
+        fd = []
+        if face_desc is not None:
+            # st.write(face_desc)
+            for i in face_desc:
+                fd.append(i)
+
+            db.collection("facedesc").add({"name": input_n, "Desc": fd})
+        else:
+            st.write("no face found")
 
 
-    # pickle.dump((face_desc, "TEST PICKLE5"), open('trainp.p','at'))
+
 
 
 if __name__ == '__main__':
@@ -131,6 +170,7 @@ if __name__ == '__main__':
     if st.page_select == 'Recognition':
         recog()
 
-#แปะ
+
 # python3 -m pip install --upgrade pip
 # python3 -m pip install --upgrade Pillow
+#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
